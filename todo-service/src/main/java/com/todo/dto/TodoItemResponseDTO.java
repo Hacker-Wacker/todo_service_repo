5d4e5ca8ab7dc9b2c44168fb8977dd75d112432c
Amos Mushumbusi alias Hacker-Wacker <amos.mushumbusi@yahoo.com>
2023-12-11 13:07:30 +0100

    Create service response DTO

---
+++ todo-service/src/main/java/com/todo/dto/TodoItemResponseDTO.java
@@ -0,0 +1,76 @@
+package com.todo.dto;
+
+import java.time.LocalDateTime;
+
+public class TodoItemResponseDTO {
+    private Long id;
+
+    private String description;
+
+    private String status;
+
+    private LocalDateTime creationDateTime;
+
+    private LocalDateTime dueDateTime;
+
+    private LocalDateTime doneDateTime;
+
+    public TodoItemResponseDTO() {}
+
+    public TodoItemResponseDTO(Long id, String description, String status, LocalDateTime creationDateTime, LocalDateTime dueDateTime, LocalDateTime doneDateTime) {
+        this.id = id;
+        this.description = description;
+        this.status = status;
+        this.creationDateTime = creationDateTime;
+        this.dueDateTime = dueDateTime;
+        this.doneDateTime = doneDateTime;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreationDateTime() {
+        return creationDateTime;
+    }
+
+    public void setCreationDateTime(LocalDateTime creationDateTime) {
+        this.creationDateTime = creationDateTime;
+    }
+
+    public LocalDateTime getDueDateTime() {
+        return dueDateTime;
+    }
+
+    public void setDueDateTime(LocalDateTime dueDateTime) {
+        this.dueDateTime = dueDateTime;
+    }
+
+    public LocalDateTime getDoneDateTime() {
+        return doneDateTime;
+    }
+    
+    public void setDoneDateTime(LocalDateTime doneDateTime) {
+        this.doneDateTime = doneDateTime;
+    }
+}
